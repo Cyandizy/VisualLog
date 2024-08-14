a339080a527333d84a40be79c2dabcff42430c03
lyns0 <cyanallybusiness@gmail.com>
2024-08-14 11:10:50 +0700

    Chore: clean ApplicationSettings.cs

--- Models/ApplicationSettings.cs
+++ Models/ApplicationSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VisualLog.Models
+﻿namespace VisualLog.Models
 {
     public static class ApplicationSettings
     {
